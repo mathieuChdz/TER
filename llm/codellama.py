12778320d6e5966dbec40b42276aff5a7590ff4f
Bryan <bryan.le-blanc@ens.uvsq.fr>
2025-03-01 18:58:56 +0000

    Modifications pour pouvoir dialoguer avec un llm en ligne de commande

--- llm/codellama.py
+++ llm/codellama.py
@@ -1,21 +1,53 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 import transformers
 import torch
+import re
 
-model_id = "codellama/CodeLlama-7b-Instruct-hf"
-tokenizer = AutoTokenizer.from_pretrained(model_id)
-model = AutoModelForCausalLM.from_pretrained(
-   model_id,
-   torch_dtype=torch.float16,
-   device_map="auto",
-)
+def ask_model():
+    models = {
+        "1": "codellama/CodeLlama-7b-Instruct-hf",
+        "2": "codellama/CodeLlama-70b-Instruct-hf"
+    }
+    print("Available models:")
+    for key, value in models.items():
+        print(f"{key}. {value.split('/')[-1]}")
+    print("")
+    model_number = input("Enter the model number: ")
+    model_id = models.get(model_number)
+    if model_id is None:
+        print("Invalid model ID.")
+        return ask_model()
+    print("")
+    return model_id
 
-chat = [
-   {"role": "user", "content": "Who are you ?"},
-]
-inputs = tokenizer.apply_chat_template(chat, return_tensors="pt")
-inputs = inputs.to("cuda")
+def load_model(model_id):
+    tokenizer = AutoTokenizer.from_pretrained(model_id)
+    model = AutoModelForCausalLM.from_pretrained(
+        model_id,
+        torch_dtype=torch.float16,
+        device_map="auto",
+    )
+    print("")
+    return model, tokenizer
 
-output = model.generate(input_ids=inputs, max_new_tokens=200)
-output = output[0].to("cpu")
-print(tokenizer.decode(output))
+def chat_with_model(model, tokenizer):
+    print("Type 'exit' or 'quit' or 'q' to stop chatting.\n")
+    while True:
+        user_input = input("User: ")
+        if user_input.lower() in ["exit", "quit", "q"]:
+            break
+        print("")
+        chat = [{"role": "user", "content": user_input},]
+        inputs = tokenizer.apply_chat_template(chat, return_tensors="pt")
+        inputs = inputs.to("cuda")
+        output = model.generate(input_ids=inputs, max_new_tokens=100000)
+        output = output[0].to("cpu")
+        response = tokenizer.decode(output, skip_special_tokens=True)
+        response = re.sub(r'\[INST\].*?\[\/INST\]', '', response).strip()
+        print(f"Model: {response}\n")
+
+
+if __name__ == "__main__":
+    model_id = ask_model()
+    model, tokenizer = load_model(model_id)
+    chat_with_model(model, tokenizer)
