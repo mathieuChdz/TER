8fee46bb52edc1faf895d300289a3c3e42022916
mathieu <chedozeaumathieu@gmail.com>
2025-03-17 18:17:22 +0000

    ajout du calcul de la conso energitique

--- Comparaison_gpu_cpu/matrix_multiplication_test.py
+++ Comparaison_gpu_cpu/matrix_multiplication_test.py
@@ -50,13 +50,13 @@ def gpu_matrix_multiplication(size):
     return C
 
 if __name__ == "__main__":
-    size = 100000
+    size = 10000
     gc.init_NVML()
     begin_power = gc.get_total_power_usage()
     result_gpu = gpu_matrix_multiplication(size)
     end_power = gc.get_total_power_usage()
     gc.stop_NVML()
     
-    result_cpu = cpu_matrix_multiplication(size)
+    # result_cpu = cpu_matrix_multiplication(size)
 
     print(f"GPU: Power consumption: {end_power - begin_power:.2f} watts")
