a950e40d36d56517706af07309936f2f366e53f9
Bryan <bryan.le-blanc@ens.uvsq.fr>
2025-04-15 10:52:11 +0000

    modifications page Rank

--- PageRank/pageRank.py
+++ PageRank/pageRank.py
@@ -1,26 +1,47 @@
+from scipy.io import mmread
+
 import cudf
 import cugraph
-from scipy.io import mmread
-from scipy.sparse import coo_matrix
 
-# Lire le fichier mtx avec scipy
-def read_mtx(matrix_file):
+import pandas as pd
+import networkx as nx
+
+import time
+import cupy as cp
+
+def create_dataframe_cpu(matrix):
     """
-    Lit un fichier Matrix Market (.mtx) et retourne un DataFrame cuDF.
+    Crée un DataFrame pandas à partir d'une matrice COO
     """
-    # Lire la matrice au format Matrix Market
-    matrix = mmread(matrix_file)
-    
-    # Créer un DataFrame cuDF
+    df = pd.DataFrame({
+        'src': matrix.row,
+        'dst': matrix.col,
+        'weight': matrix.data
+    })
+    return df
+
+def create_dataframe_gpu(matrix):
+    """
+    Crée un DataFrame cuDF à partir d'une matrice COO.
+    """
     df = cudf.DataFrame({
         'src': matrix.row,
         'dst': matrix.col,
         'weight': matrix.data
     })
-    print(f"Nombre d'arêtes avec poids nul : {len(df[df['weight'] == 0])}")
     return df
 
-def create_graph(edges):
+def create_graph_cpu(edges):
+    """
+    Crée un graphe NetworkX à partir d'un DataFrame pandas contenant les arêtes.
+    """
+    G = nx.DiGraph()
+    for _, row in edges.iterrows():
+        G.add_edge(row['src'], row['dst'], weight=row['weight'])
+    return G
+
+
+def create_graph_gpu(edges):
     """
     Crée un graphe à partir d'un DataFrame cuDF contenant les arêtes.
     """
@@ -28,12 +49,29 @@ def create_graph(edges):
     G.from_cudf_edgelist(edges, source='src', destination='dst', edge_attr='weight')
     return G
 
-matrix = "webbase-1M.mtx"
+def page_rank_cpu(matrix):
+    G = create_graph_cpu(create_dataframe_cpu(matrix))
 
-G = create_graph(read_mtx("matrices/" + matrix))
+    start_time = time.time()
+    nx.pagerank(G, alpha=0.85, max_iter=100, tol=1e-5)
+    end_time = time.time()
 
-# Affichage des arêtes pour test
-print(G.view_edge_list())
+    return end_time - start_time
 
-# Calcul de PageRank
-# cugraph.pagerank(G, alpha=0.85, max_iter=100, tol=1e-5)
+def page_rank_gpu(matrix):
+    G = create_graph_gpu(create_dataframe_gpu(matrix))
+
+    start_time = time.time()
+    cugraph.pagerank(G, alpha=0.85, max_iter=100, tol=1e-5)
+    cp.cuda.Stream.null.synchronize()
+    end_time = time.time()
+
+    return end_time - start_time
+
+
+
+if __name__ == "__main__":
+    matrix_file = "matrices/webbase-1M.mtx"
+    matrix = mmread(matrix_file)
+    print(f'PageRank CPU Time: {page_rank_cpu(matrix)}s')
+    print(f'PageRank GPU Time: {page_rank_gpu(matrix)}s')
